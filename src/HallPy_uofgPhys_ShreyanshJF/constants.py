72bf5fd3e664bdd00c1f8a4079fca1fa1951a7ee
Shreyansh Fofandi <shreyanshjf@gmail.com>
2022-07-29 15:23:06 +0100

    Updated support for GDM8342 Multimeter - 0.0.102

--- src/HallPy_uofgPhys_ShreyanshJF/constants.py
+++ src/HallPy_uofgPhys_ShreyanshJF/constants.py
@@ -1,6 +1,6 @@
 
 supportedInstruments = {
-    "Power Supply": ["TENMA 72-2710", "GDM8324"],
+    "Power Supply": ["TENMA 72-2710", "GWInstek,GDM8342"],
     "LCR Meter": ["B&K Precision ,891"],
     "Multimeter": ["KEITHLEY INSTRUMENTS INC.,MODEL 2110"]
 }
@@ -9,7 +9,7 @@ supportedInstruments = {
 
 serialRegex = {
     "TENMA 72-2710": r"SN:[0-9]{8}",
-    "GDM8324": r"[0-9]{8}",
+    "GWInstek,GDM8342": r"[A-Z]{3}[0-9]{6}",
     "B&K Precision ,891": r"[0-9A-Z]{9}",
     "KEITHLEY INSTRUMENTS INC.,MODEL 2110": r"[0-9]{7}"
 }
